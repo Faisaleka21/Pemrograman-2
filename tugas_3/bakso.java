9d67fcd3db4f7ef0c3a9a831cbb795bf8ae35d6a
Faisaleka21 <162295533+Faisaleka21@users.noreply.github.com>
2024-11-18 17:13:14 +0700

    Add files via upload

---
+++ tugas_3/bakso.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas_3;
+
+/**
+ *
+ * @author Fysl
+ */
+public class bakso extends makanan{
+    void mercon(){
+        System.out.println("Bakso Mercon");
+    }
+    
+    void beranak(){
+        System.out.println("Bakso Beranak");
+    }
+    
+}
---
+++ tugas_3/makanan.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas_3;
+
+/**
+ *
+ * @author Fysl
+ */
+public class makanan {
+      
+      int harga;
+      String nama;
+      
+      void beli(){
+          System.out.println("---------------------");
+          System.out.println("Makan disini!!!");
+      }
+      void proses(){
+          System.out.println("---------------------");
+          System.out.println("Pesanan sedang di Proses!!");
+      }
+      void terima(){
+          System.out.println("---------------------");
+          System.out.println("Pesanan di terima!");
+      }
+      void baru(){
+          System.out.println("---------------------");
+          System.out.println("Pesanan Baru");
+      }
+      void kirim(){
+          System.out.println("---------------------");
+          System.out.println("Pesanan Dikirimkan!!");
+      }
+    
+}
