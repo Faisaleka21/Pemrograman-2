1c55ce48501d908f98780a2ff4d54c079e3cdeb7
Faisaleka21 <162295533+Faisaleka21@users.noreply.github.com>
2024-11-18 17:20:47 +0700

    Add files via upload

---
+++ tugas_2/method.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas_2;
+
+/**
+ *
+ * @author Fysl
+ */
+public class method {
+    //prosedur,parameter
+    public void hasil(String nama, int nilai){
+        String ket = keterangan(nilai);
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("||\t Nama : "+nama);
+        System.out.println("||\t Nilai : "+nilai);
+        System.out.println("||\t Keterangan : "+ket);
+        System.out.println("=====================================================================");
+        System.out.println("Nama Mahasiswa "+nama+ " ,dinyatakan "+ket+" karena mendapat nilai "+nilai+".");
+        System.out.println("=====================================================================");
+    }
+    
+    //fungsi dan modifier
+    private String keterangan(int x){
+    if (x>70){
+    return "Lulus";
+        }
+    else {return "Tidak Lulus";
+        }
+    }
+
+}
